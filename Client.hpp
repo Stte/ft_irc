e547b0c788b5afb69078c83a1eabf1f5794e9d3e
Dean Ruina HIVE computer <ruinadd@gmail.com>
2024-03-04 12:43:46 +0200

    main, Server, Client class

---
+++ Client.hpp
@@ -0,0 +1,25 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <iostream>
+#include <vector> //-> for vector
+#include <sys/socket.h> //-> for socket()
+#include <sys/types.h> //-> for socket()
+#include <netinet/in.h> //-> for sockaddr_in
+#include <fcntl.h> //-> for fcntl()
+#include <unistd.h> //-> for close()
+#include <arpa/inet.h> //-> for inet_ntoa()
+#include <poll.h> //-> for poll()
+#include <csignal> //-> for signal()
+
+class Client
+{
+	private:
+
+
+	public:
+		Client();
+		~Client();
+};
+
+#endif
---
+++ Server.hpp
@@ -0,0 +1,25 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <iostream>
+#include <vector> //-> for vector
+#include <sys/socket.h> //-> for socket()
+#include <sys/types.h> //-> for socket()
+#include <netinet/in.h> //-> for sockaddr_in
+#include <fcntl.h> //-> for fcntl()
+#include <unistd.h> //-> for close()
+#include <arpa/inet.h> //-> for inet_ntoa()
+#include <poll.h> //-> for poll()
+#include <csignal> //-> for signal()
+
+class Server
+{
+	private:
+
+	public:
+		Server(std::string port, std::string &password);
+		~Server();
+
+};
+
+#endif
