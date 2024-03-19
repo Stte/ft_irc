ed8d7c625f7896faf238b1519d2103d4789b3331
Doxakis1 <mike.karat@yahoo.com>
2024-03-19 11:37:29 +0200

    Moved Message functions to their cpp file

--- src/Message.cpp
+++ src/Message.cpp
@@ -11,3 +11,46 @@ IRCCommand assignCommand(std::string cmd)
     }
     return IRCCommand::ERROR;
 }
+
+Message::Message(const std::string& msg) : rawMessage(msg) {
+    Message::parse();
+}
+
+std::string Message::getPrefix() const { return prefix; }
+IRCCommand Message::getCommand() const { return command; }
+std::vector<std::string> Message::getParams() const { return params; }
+const std::string &Message::getRawCmd(){ return rawCmd; }
+
+void Message::parse()
+{
+        // std::cout << rawMessage << std::endl;
+        size_t prefixEnd = 0;
+        if (rawMessage[0] == ':') {
+            prefixEnd = rawMessage.find(' ');
+            prefix = rawMessage.substr(1, prefixEnd - 1);
+            prefixEnd++;
+        } // this might not necessarily exist as per irc rules, prefixes are not mandatory
+
+        // Find command
+        size_t commandEnd = rawMessage.find(' ', prefixEnd);
+        rawCmd = rawMessage.substr(prefixEnd, commandEnd - prefixEnd);
+        command = assignCommand(rawCmd);
+        std::cout << "cmd" << rawCmd << std::endl;
+        // if (command == IRCCommand::ERROR)
+        //     throw something maybe
+        size_t start = commandEnd + 1;
+        
+        while (start < rawMessage.length()) {
+            // std::cout << "start is: " << start << std::endl;
+            size_t end;
+            if (rawMessage[start] == ':') {
+                end = rawMessage.length(); // ':' indicates always end of message so ':' to last character is last parameter
+            } else {
+                end = rawMessage.find(' ', start); // for example in case: :Teemu KICK #channel Dean :Reason for kick
+                if (end == rawMessage.npos)
+                    end = rawMessage.length();
+            }
+            params.push_back(rawMessage.substr(start, end - start));
+            start = end + 1;
+       }
+}
